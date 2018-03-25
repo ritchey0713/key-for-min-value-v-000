2055605bfc80c62dfb17ee9518fdde26e9f3f601
David Ritchey <ritchey0713@gmail.com>
2018-03-25 16:51:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
    new_value = 0 
   name_hash.each do |name, value|
-    if new_value < value 
+    if new_value > value 
       return name 
     else 
       nil 
